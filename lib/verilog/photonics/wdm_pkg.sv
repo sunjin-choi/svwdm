389a1cc4c2be9d5c1fef7d459f7fe2141e26f845
Sunjin Choi <sunjin_choi@berkeley.edu>
2025-06-18 04:57:11 -0700

    clean up

--- lib/verilog/photonics/wdm_pkg.sv
+++ lib/verilog/photonics/wdm_pkg.sv
@@ -14,14 +14,6 @@ package wdm_pkg;
   `define DEFINE_WAVES_TYPE(WIDTH) \
     typedef struct { wave_t wave_bundle[WIDTH-1:0]; } waves``WIDTH``_t;
 
-  /*  typedef struct {wave_t wave_bundle[0:0];} waves1_t;
- *
- *  typedef struct {wave_t wave_bundle[3:0];} waves4_t;
- *
- *  typedef struct {wave_t wave_bundle[7:0];} waves8_t;
- *
- *  typedef struct {wave_t wave_bundle[15:0];} waves16_t;*/
-
   `DEFINE_WAVES_TYPE(1)
   `DEFINE_WAVES_TYPE(4)
   `DEFINE_WAVES_TYPE(8)
@@ -30,7 +22,6 @@ package wdm_pkg;
   `define DECLARE_WAVES_TYPE(WIDTH) \
     typedef waves``WIDTH``_t WAVES_TYPE; \
     localparam int WAVES_WIDTH = WIDTH; \
-
 
 endpackage
 
