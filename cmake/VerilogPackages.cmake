1869b3e538ab5c89ee263bc85122a70eec7e012b
Sunjin Choi <sunjin_choi@berkeley.edu>
2025-07-06 21:18:29 -0700

    remove caching of cmake var, fix comment

--- cmake/VerilogPackages.cmake
+++ cmake/VerilogPackages.cmake
@@ -1,8 +1,7 @@
-# Define project-wide package ordering for Verilog sources
-# Packages should be listed relative to VERILOG_LIB_DIR so paths remain portable.
+# Define project-wide package ordering for Verilog sources. Packages should be
+# listed with full paths, constructed using VERILOG_LIB_DIR.
 
 set(VERILOG_PACKAGE_ORDER
     "${VERILOG_LIB_DIR}/photonics/wdm_pkg.sv"
     "${VERILOG_LIB_DIR}/tuner/tuner_pkg.sv"
-    "${VERILOG_LIB_DIR}/tuner/tuner_phy_pkg.sv"
-    CACHE STRING "Ordered list of Verilog package files")
+    "${VERILOG_LIB_DIR}/tuner/tuner_phy_pkg.sv")
